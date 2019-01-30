051b5cc50dc829dc96afea353e7a2f10a73f04ce
Jørgen <fugla-gutt@hotmail.com>
2019-01-30 14:41:54 +0100

    Add more fields to car

--- car.py
+++ car.py
@@ -1,9 +1,14 @@
+from random import uniform
 class Car:
+    reactionTime = 3
     # A car needs to have an id, position and speed
-    def __init__(self, id, position, velocity = (0,0)):
+
+    def __init__(self, id, position, velocity = (0,0), lane = None):
         self.id = id
         self.position = position
         self.velocity = velocity
+        self.reactionTime = uniform(1,self.reactionTime)
+        self.lane = lane
 
     def getVelocity(self):
         return self.velocity
@@ -16,5 +21,17 @@ class Car:
 
     def setPosition(self, postion):
         self.position = postion
+
+    def getReactionTime(self):
+        return self.reactionTime
+
+    def setReactionTime(self, reactionTime):
+        self.reactionTime = reactionTime
+
+    def getLane(self):
+        return self.lane
+
+    def setLane(self, lane):
+        self.lane = lane
 
     # TODO: We could add types of cars, and thus have pictures that match them. Size might also be needed
